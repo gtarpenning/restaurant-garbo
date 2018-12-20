c5559f0ea47fc6a1a628493e63e964fec8f22643
Alex Polissky <apolissky@gmail.edu>
2018-12-19 16:50:13 -0800

    linter fuckery

--- basicInfo.py
+++ basicInfo.py
@@ -2,13 +2,12 @@
 import util
 import requests as req
 import pandas as pd
-import numpy as np
-from tqdm import tqdm
 
 
 LOC = 'San Francisco'
 TERM = 'Mexican'
 API_KEY = 'RERrcqtrW3V1ARX5kr_3VC9H8DnboL7bkyVf5HdQ-XiRi-hPm2jX_TRBUuc8lDFtYKKF2B_PT1fDai2hsVwCdirNc5Hrk7k9krlw7Vt---u9tZTXjbDZGRyYAxnOW3Yx'
+
 
 def get_rest_info(id):
     infoBin = []
@@ -21,16 +20,19 @@ def get_rest_info(id):
     restHours = {}
     for day in days[0]['open']:
         dayHours = (day['start'], day['end'])
-        restHours[day['day']]= dayHours
-    coords = (response['coordinates']['latitude'],response['coordinates']['longitude'])
+        restHours[day['day']] = dayHours
+    coords = (response['coordinates']['latitude'],
+              response['coordinates']['longitude'])
     categories = []
     for category in response['categories']:
         categories.append(category["title"])
     phone = response['phone']
     isClaimed = response['is_claimed']
     transactions = response['transactions']
-    infoBin += [price, restHours, coords, categories, phone, isClaimed, transactions]
+    infoBin += [price, restHours, coords, categories, phone,
+                isClaimed, transactions]
     return infoBin
+
 
 def make_df_for_rests(loc, term, numRestaurants):
     restaurants = util.get_top_yelp(loc, term, numRestaurants)['businesses']
@@ -39,15 +41,18 @@ def make_df_for_rests(loc, term, numRestaurants):
     for rest in restaurants:
         restNames.append(rest['name'])
         restInfo.append(get_rest_info(rest['id']))
-    axes = ['price', 'hours', 'coordinates', 'categories', 'phone', 'isClaimed', 'transactions']
-    df = pd.DataFrame.from_dict(dict(zip(restNames,restInfo)),
-        orient='index', columns=axes)
+    axes = ['price', 'hours', 'coordinates', 'categories', 'phone',
+            'isClaimed', 'transactions']
+    df = pd.DataFrame.from_dict(dict(zip(restNames, restInfo)),
+                                orient='index', columns=axes)
     return df
+
 
 def main():
     numRestaurants = 5
     df1 = make_df_for_rests(LOC, TERM, numRestaurants)
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
         print(df1)
+
 
 main()
