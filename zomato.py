75d9de56d39efc49cb15999ae6e8b09d31de0bca
gtarpenning <gtarpenning@gmail.com>
2018-12-20 14:39:16 -0800

    smol changes

---
+++ zomato.py
@@ -0,0 +1,39 @@
+import requests as req
+import json
+import pandas as pd
+
+
+API_KEY = 'd355c02b0770fb0b64bd3e6721686b91'
+CITY = 'San Francisco'
+TERM = 'Sushi'
+
+baseLink = "https://developers.zomato.com/api/v2.1/"
+h = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user_key": API_KEY}
+
+
+def get_zomato_data(city, term):
+    r = req.get(baseLink + 'cities?q=' + city.replace(' ', '%20'), headers=h)
+    cityID = str(json.loads(r.text)['location_suggestions'][0]['id'])
+
+    r = req.get(baseLink + 'search?city_id=' + cityID + '&q=' + term, headers=h)
+    response = json.loads(r.text)
+
+    rBin = []
+    for restaurant in response['restaurants']:
+        r = restaurant['restaurant']
+        a_cost = r['average_cost_for_two']
+        rating = r['user_rating']['aggregate_rating']
+        votes = r['user_rating']['votes']
+        o_support = r['opentable_support']
+        rBin.append([r['name'], rating, votes, a_cost, o_support])
+
+    cols = ['name', 'rating', 'votes', 'average cost for 2', 'open table']
+    return pd.DataFrame(data=rBin, columns=cols)
+
+
+def main():
+    print(get_zomato_data(CITY, TERM))
+
+
+if __name__ == '__main__':
+    main()
